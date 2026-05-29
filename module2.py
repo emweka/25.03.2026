88c61bd935615534c6c861b095692426a5475b30
emweka <vankakolbasin@icloud.com>
2026-05-29 13:09:04 +0200

    module2.py

--- module2.py
+++ module2.py
@@ -1,1 +1,6 @@
-
+def invert_case_in_file(input_filename, output_filename):
+    with open(input_filename, 'r', encoding='utf-8') as infile:
+        text = infile.read()
+    transformed_text = text.swapcase()
+    with open(output_filename, 'w', encoding='utf-8') as outfile:
+        outfile.write(transformed_text)
