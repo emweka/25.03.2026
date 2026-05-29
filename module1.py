f7899190901602cee9f1b8dafec6619ef233079a
emweka <vankakolbasin@icloud.com>
2026-05-29 13:08:01 +0200

    module1.py

--- module1.py
+++ module1.py
@@ -1,1 +1,21 @@
+def write_numbers_to_file(filename, numbers):
+    with open(filename, 'w', encoding='utf-8') as f:
+        for num in numbers:
+            f.write(str(num) + '\n')
 
+def read_numbers_from_file(filename):
+    numbers = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                numbers.append(float(line))
+    return numbers
+
+def compute_product(numbers):
+    if not numbers:
+        return 1.0
+    prod = 1.0
+    for num in numbers:
+        prod *= num
+    return prod
